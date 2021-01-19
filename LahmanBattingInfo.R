7a0a9e805ab4cc9ff54ba282fe33e8702522ca54
WoutDavid <wouters.david@hotmail.com>
2021-01-19 14:59:51 +0100

    added platoon effect data

--- LahmanBattingInfo.R
+++ LahmanBattingInfo.R
@@ -1,10 +1,49 @@
+####################################
+## Confirming the Platoon Effect ###
+####################################
+
+######################
+# Preparing the data #
+######################
+
+library(dplyr)
+#importing the data I webscraped from mlb.com/stats/
+versusLeft <- read.csv("data/versusLeft.csv")
+versusRight <- read.csv("data/versusRight.csv")
+versusLeft <- versusLeft[,c("FIRSTNAME", "LASTNAME", "OBP")]
+versusRight <- versusRight[,c("FIRSTNAME", "LASTNAME", "OBP")]
+
+##This data doesn't have the batting hand yet, for that we merge with Lahman database
 library(Lahman)
 ##contains all batters
 data(Batting)
-unique(Batting$yearID)
 Batting <- Batting[Batting$yearID==2019,]
 ##contains bathand info and names
 data(Master)
 merged <- merge(Batting, Master)
-merged <- merged[,c("yearID", "bats", "nameFirst", "nameLast")]
+merged <- merged[,c("bats","nameFirst","nameLast")]
+colnames(merged)[2] <- "FIRSTNAME"
+colnames(merged)[3] <- "LASTNAME"
+df = merged[!duplicated(merged$LASTNAME, merged$LASTNAME),]
 
+withBattingHand <- merge(versusLeft, df, by = c("LASTNAME", "FIRSTNAME"))
+battingVersusLeft <- withBattingHand[!duplicated(withBattingHand$LASTNAME, withBattingHand$LASTNAME),]
+
+withBattingHand <- merge(versusRight, df, by = c("LASTNAME", "FIRSTNAME"))
+battingVersusRight <- withBattingHand[!duplicated(withBattingHand$LASTNAME, withBattingHand$LASTNAME),]
+
+#creating a list of dataframes
+dfList <- NULL
+dfList[[1]] = battingVersusLeft
+dfList[[2]] = battingVersusRight
+library(data.table)
+boundBatting <- rbindlist(dfList, idcol = "Origin")
+boundBatting
+##index 1 = agains left, index 2 = right
+
+##################################
+## Exploring the platoon effect ##
+##################################
+par(mfrow=c(1,2))
+boxplot(OBP ~ bats, battingVersusLeft)
+boxplot(OBP ~ bats, battingVersusRight)
